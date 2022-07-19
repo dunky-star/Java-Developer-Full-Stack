7898fb836b5fb447ea98f7ae6cf1ea90bea48fec
dunky-star <dukygeoffrey39@gmail.com>
2022-07-19 06:29:55 +0300

    primitve data types

---
+++ phase1/PrimitiveDataTypes/src/PrimitiveDataTypes.java
@@ -0,0 +1,17 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args){
+
+        boolean k = false;            // boolean = 1 bit (true/false).
+        byte byteValue = 100;         // byte = 8 bits, range: -128 to 127.
+        short shortValue = -30000;    // short = 2 bytes, range: -32768 to 32767.
+        int intValue = 1000000;       // int = 4 bytes, range: -2^32 to (2^31) - 1.
+        long myLong = 9000;           // long = 8 bytes, Range: -2^63 to (2^63) - 1.
+        float myFloat = 2.5F;         // float = 4 bytes.
+        double myDouble = 2.8;        // double = 8 bytes.
+        char myChar = 'M';            // char = 1 bytes, it also stores a single character
+                                      // a minimum value of '\u0000' and a maximum value of '\uffff'.
+
+
+
+    }
+}
