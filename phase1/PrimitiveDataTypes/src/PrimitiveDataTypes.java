e1cfff36a2d38a05d5195f71ffeb4a0de4f74508
dunky-star <dukygeoffrey39@gmail.com>
2022-07-19 14:00:34 +0300

    Type Casting Concept

--- phase1/PrimitiveDataTypes/src/PrimitiveDataTypes.java
+++ phase1/PrimitiveDataTypes/src/PrimitiveDataTypes.java
@@ -5,7 +5,7 @@ public class PrimitiveDataTypes {
         byte byteValue = 100;         // byte = 8 bits, range: -128 to 127.
         short shortValue = -30000;    // short = 2 bytes, range: -32768 to 32767.
         int intValue = 1000000;       // int = 4 bytes, range: -2^32 to (2^31) - 1.
-        long myLong = 9000;           // long = 8 bytes, Range: -2^63 to (2^63) - 1.
+        long myLong = 9000L;           // long = 8 bytes, Range: -2^63 to (2^63) - 1.
         float myFloat = 2.5F;         // float = 4 bytes.
         double myDouble = 2.8;        // double = 8 bytes.
         char myChar = 'M';            // char = 1 bytes, it also stores a single character
---
+++ phase2/TypeCasting/src/TypeCastingJava.java
@@ -0,0 +1,14 @@
+/* Geoffrey Duncan Opiyo
+ * Widening Casting (automatically) - converting a smaller type to a larger type size.
+ * byte -> short -> char -> int -> long -> float -> double.
+ * Narrowing Casting (manually) - converting a larger type to a smaller size type.
+ * double -> float -> long -> int -> char -> short -> byte
+ */
+public class TypeCastingJava {
+    public static void main(String[] args){
+        int intValue = 190;
+
+        double doubleValue = intValue;  // Automatic casting: int to double.
+        System.out.println("\nThe casted int value " + intValue + " to double = " + doubleValue );
+    }
+}
