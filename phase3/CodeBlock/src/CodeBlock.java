1fe1b67214152da73a9b6b993fe809ec2d05269c
dunky-star <dukygeoffrey39@gmail.com>
2022-07-22 12:03:53 +0300

    Code block

--- phase3/CodeBlock/src/CodeBlock.java
+++ phase3/CodeBlock/src/CodeBlock.java
@@ -10,5 +10,15 @@ public class CodeBlock {
             System.out.println("\nYour final score was: " + score);
         }
 
+        boolean newGameOver = true;
+        int newScore = 20000;
+        int newLevelCompleted = 8;
+        int newBonus = 250;
+        if (newGameOver){
+            int finalScore = newScore + (newLevelCompleted * newBonus);
+            System.out.println("\nYour final score was: " + newScore);
+        }
+
+
     }
 }
