5212bc9b64d5b2b426b91461844dc1c69096ab87
dunky-star <dukygeoffrey39@gmail.com>
2022-07-22 14:27:08 +0300

    Java methods

--- phase3/CodeBlock/src/CodeMethod.java
+++ phase3/CodeBlock/src/CodeMethod.java
@@ -1,21 +1,20 @@
 public class CodeMethod {
     public static void main(String[] args) {
 
-        boolean gameOver = true;
         int score = 0;
         int levelCompleted = 8;
         int bonus = 250;
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("\nYour final score was: " + finalScore);
-        }
+
         calculateScore(true, 2000, levelCompleted, bonus);
+
+        calculateScore(true, 0, 8, 250);
 
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 0;
             System.out.println("\nYour final score was: " + finalScore);
         }
 
