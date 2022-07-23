099db826a4a15e72ae11740e12688248b9a9b0f8
dunky-star <dukygeoffrey39@gmail.com>
2022-07-23 18:37:27 +0300

    Java collections, Lambda expressions and Functional interface

--- phase9/FunctionalInterfaceJava/src/phase9/FunctionalFilterJ.java
+++ phase9/FunctionalInterfaceJava/src/phase9/FunctionalFilterJ.java
@@ -23,19 +23,25 @@ public class FunctionalFilterJ {
         System.out.println("\nList of Students: " + listOfStudents); // Printing array list.
 
         // Using one filter()
-        System.out.println("\nPrint list of student whose length is greater than 10");
-        System.out.println("======================================================");
+        System.out.println("\nlist of student print whose length is greater than 10");
+        System.out.println("========================================================");
         listOfStudents.stream()
                 .filter(s -> s.length() > 10)
                 .forEach(System.out::println);
-        System.out.println(" ");
+
+        System.out.println("\nPrint first name which has the letter 'e'");
+        System.out.println("==========================================");
+        String First = listOfStudents.stream()
+                .filter(s -> s.contains("e"))
+                .findFirst().get();
+        System.out.print(First + "\n");
 
         List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 12, 18, 20, 22); // Creating arrayList Int type for data storage.
         Integer lowestCommonMultiple = listOfNumbers.stream()
                 .filter(i -> i % 2 == 0)
                 .filter(i -> i % 3 == 0)
                 .findFirst().get(); // To find first number from filtered list.
-        System.out.print("The first lowest LCM from the integer list is: " + lowestCommonMultiple + "\n");
+        System.out.println("\nThe first lowest LCM from the integer list is: " + lowestCommonMultiple + "\n");
 
         Stream<Integer> divisibleByThree = listOfNumbers.stream()
                 .filter(x -> x % 2 != 0);
