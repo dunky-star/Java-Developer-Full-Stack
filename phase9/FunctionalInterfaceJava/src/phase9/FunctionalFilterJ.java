d26e3040158e2a88d1392bf9195784228ccc36aa
dunky-star <dukygeoffrey39@gmail.com>
2022-07-23 11:11:08 +0300

    Java collections, Lambda expressions and Functional interface

--- phase9/FunctionalInterfaceJava/src/phase9/FunctionalFilterJ.java
+++ phase9/FunctionalInterfaceJava/src/phase9/FunctionalFilterJ.java
@@ -19,14 +19,14 @@ public class FunctionalFilterJ {
         listOfStudents.add("Murphy Laws");
         listOfStudents.add("Shaka John");
         listOfStudents.add("Optimus Prime");
-        System.out.println(listOfStudents); // Printing array list.
+        System.out.println("\nList of Students: " + listOfStudents); // Printing array list.
 
         List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 12, 18, 20, 22); // Creating arrayList Int type for data storage.
         Integer lowestCommonMultiple = listOfNumbers.stream()
                                    .filter(i -> i % 2 == 0)
                                    .filter(i -> i % 3 == 0)
                                    .findFirst().get(); // To find first number from filtered list.
-        System.out.print(lowestCommonMultiple);
+        System.out.print("The first lowest LCM from the integer list is: " + lowestCommonMultiple + "\n");
 
 
     }
