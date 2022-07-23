57c24ff12f95539140a64b7a944a563459a9a7ee
dunky-star <dukygeoffrey39@gmail.com>
2022-07-23 10:56:04 +0300

    Java collections, Lambda expressions and Function interface

--- phase9/FunctionalInterfaceJava/src/phase9/FunctionalFilterJ.java
+++ phase9/FunctionalInterfaceJava/src/phase9/FunctionalFilterJ.java
@@ -1,10 +1,16 @@
+/**
+ * Written by: Geoffrey Duncan O.
+ * Sample Java program to demonstrate how to use the filter() on Java collections.
+ */
 package phase9;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FunctionalFilterJ {
     public static void main(String[] args){
-        List<String> listOfStudents = new ArrayList<>(); // Creating arrayList for data storage.
+        List<String> listOfStudents = new ArrayList<>(); // Creating arrayList String type for data storage.
         listOfStudents.add("Christine"); // Storing data in arrayList.
         listOfStudents.add("Duncan");
         listOfStudents.add("Anajana");
@@ -13,7 +19,15 @@ public class FunctionalFilterJ {
         listOfStudents.add("Murphy Laws");
         listOfStudents.add("Shaka John");
         listOfStudents.add("Optimus Prime");
-        System.out.println(listOfStudents);
+        System.out.println(listOfStudents); // Printing array list.
+
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 12, 18, 20, 22); // Creating arrayList Int type for data storage.
+        Integer lcm = listOfNumbers.stream()
+                                   .filter(i -> i % 2 == 0)
+                                   .filter(i -> i % 3 == 0)
+                                   .findFirst().get();
+        System.out.print(lcm);
+
 
     }
 
