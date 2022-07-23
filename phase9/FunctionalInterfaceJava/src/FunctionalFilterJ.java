9d170fc5449420b04ad1642910a743744288095e
dunky-star <dukygeoffrey39@gmail.com>
2022-07-23 08:36:20 +0300

    Functional Interfaces in Java with Java collections

---
+++ phase9/FunctionalInterfaceJava/src/FunctionalFilterJ.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FunctionalFilterJ {
+    public static void main(String[] args){
+        List<String> listOfStudents = new ArrayList<>(); // Creating arrayList for data storage.
+        listOfStudents.add("Christine"); // Storing data in arrayList.
+        listOfStudents.add("Duncan");
+        listOfStudents.add("Anajana");
+        listOfStudents.add("Deus Kanshabe");
+        listOfStudents.add("Sheena A");
+        listOfStudents.add("Murphy Laws");
+        listOfStudents.add("Shaka John");
+        listOfStudents.add("Optimus Prime");
+        System.out.println(listOfStudents);
+
+    }
+
+
+}
