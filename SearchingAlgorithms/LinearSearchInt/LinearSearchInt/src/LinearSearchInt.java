6c61d60c2b63ae81d3bd6f235999b2a9290cb7ae
dunky-star <dukygeoffrey39@gmail.com>
2022-07-24 09:33:40 +0300

    Linear Search Algo - Integer

--- SearchingAlgorithms/LinearSearchInt/LinearSearchInt/src/LinearSearchInt.java
+++ SearchingAlgorithms/LinearSearchInt/LinearSearchInt/src/LinearSearchInt.java
@@ -9,10 +9,19 @@ public class LinearSearchInt {
         }
         return -1;
     }
-
-    public static void main(String[] args) {
-
+    // A simple helper method to print search output.
+    public static void print(int elementToSearch, int index){
+        if(index == -1){
+            System.out.println(elementToSearch + " not found");
+        }else{
+            System.out.println(elementToSearch + " found at index: " + index);
+        }
     }
 
 
+    public static void main(String[] args) {
+        int intArr[] = {89, 57, 91, 47, 95, 3, 27, 22, 67, 99};
+        int index = LinearSearchAlgo(intArr, 99); // Function call for search.
+        print(99, index); // function call to print the search result.
+    }
 }
