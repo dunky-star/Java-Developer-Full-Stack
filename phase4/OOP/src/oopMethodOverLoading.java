19f54478f32888387c8863d8a14dfca49097968c
dunky-star <dukygeoffrey39@gmail.com>
2022-07-26 21:07:49 +0300

    Method overloading Java

--- phase4/OOP/src/oopMethodOverLoading.java
+++ phase4/OOP/src/oopMethodOverLoading.java
@@ -1,14 +1,24 @@
 /**
  * Method overloading in Java.
- * Basically, using same method name but different number of parameters.
+ * Basically, using the same method name but different number of parameters.
  */
 public class oopMethodOverLoading {
-    public static int caclculateScore(String playerName, int score){
-        System.out.println("Player " + playerName + " scored " + score);
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points.");
         return score * 1000; // 1000 is an arbitrary number.
     }
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored " + score + " points." );
+        return score * 1000; // 1000 is an arbitrary number.
+    }
+    public static int calculateScore(){
+        System.out.println("No player name, no player score");
+        return 0; // 1000 is an arbitrary number.
+    }
     public static void main(String[] args){
-     int newScore =   caclculateScore("Duncan", 200);
+     int newScore =   calculateScore("Duncan", 200);
      System.out.println("New score is: " +newScore);
+     calculateScore(600);
+     calculateScore();
     }
 }
