fc859f6438de9748daddf8d3ec68ecda6aacc3d5
dunky-star <dukygeoffrey39@gmail.com>
2022-07-26 18:45:39 +0300

    Method overloading Java

--- phase4/OOP/src/oopMethodOverLoading.java
+++ phase4/OOP/src/oopMethodOverLoading.java
@@ -3,7 +3,7 @@
  */
 public class oopMethodOverLoading {
     public static int caclculateScore(String playerName, int score){
-        System.out.println("Player " + playerName + "scored " + score);
+        System.out.println("Player " + playerName + " scored " + score);
         return score * 1000;
     }
     public static void main(String[] args){
