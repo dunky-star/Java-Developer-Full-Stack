5aa2c100ec18cb3ae05594d727e51f2bb8868f32
dunky-star <dukygeoffrey39@gmail.com>
2022-07-26 20:09:53 +0300

    Method overloading Java

--- phase4/OOP/src/oopMethodOverLoading.java
+++ phase4/OOP/src/oopMethodOverLoading.java
@@ -1,12 +1,14 @@
 /**
  * Method overloading in Java.
+ * Basically, using same method name but different number of parameters.
  */
 public class oopMethodOverLoading {
     public static int caclculateScore(String playerName, int score){
         System.out.println("Player " + playerName + " scored " + score);
-        return score * 1000;
+        return score * 1000; // 1000 is an arbitrary number.
     }
     public static void main(String[] args){
-        caclculateScore("Duncan", 200);
+     int newScore =   caclculateScore("Duncan", 200);
+     System.out.println("New score is: " +newScore);
     }
 }
