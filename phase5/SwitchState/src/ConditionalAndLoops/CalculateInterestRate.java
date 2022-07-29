04259209e351f54f0e1b5ced662726a609921f11
dunky-star <dukygeoffrey39@gmail.com>
2022-07-29 20:32:04 +0300

    Conditional statements and control flows in Java

--- phase5/SwitchState/src/ConditionalAndLoops/CalculateInterestRate.java
+++ phase5/SwitchState/src/ConditionalAndLoops/CalculateInterestRate.java
@@ -7,7 +7,7 @@ public class CalculateInterestRate {
 
     public static void main(String[] args) {
         for (int i = 2; i < 9; i++) {
-            System.out.println("10000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+            System.out.println("10000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
         }
     }
 }
