9e579afdb86bd57afa64867e6708ab9bec37f5c1
dunky-star <dukygeoffrey39@gmail.com>
2022-07-29 13:00:40 +0300

    Control flow statements

--- phase5/SwitchState/src/IfStatementBasic.java
+++ phase5/SwitchState/src/IfStatementBasic.java
@@ -1,6 +1,13 @@
 public class IfStatementBasic {
     public static void main (String[] args){
-        
+        int value = 2;
+        if ( value == 1){
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else{
+            System.out.println("Value was 3");
+        }
     }
 
 }
