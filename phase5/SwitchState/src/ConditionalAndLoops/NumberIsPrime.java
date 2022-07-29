00d248bfc608980aa0fda3e7493c209070fdfa4f
dunky-star <dukygeoffrey39@gmail.com>
2022-07-29 20:47:53 +0300

    Conditional statements and control flows in Java

--- phase5/SwitchState/src/ConditionalAndLoops/NumberIsPrime.java
+++ phase5/SwitchState/src/ConditionalAndLoops/NumberIsPrime.java
@@ -6,13 +6,13 @@ public class NumberIsPrime {
             return false;
         }
         for(int i =2; i <= n/2; i++){
-            if(i%2 == 0){
+            if(n%i == 0){
                 return false;
             }
         }
         return true;
     }
     public static void main(String[] args){
-      System.out.println("The is: " +isPrime(5));
+      System.out.println("Is number prime: " +isPrime(5));
     }
 }
