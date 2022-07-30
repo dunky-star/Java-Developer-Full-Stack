1e701ed3d1c14a9f757cc3fac681c5a6a108b999
dunky-star <dukygeoffrey39@gmail.com>
2022-07-30 12:43:29 +0300

    Conditional statements and control flows in Java

---
+++ phase5/SwitchState/src/ConditionalAndLoops/WhileLoopJ.java
@@ -0,0 +1,11 @@
+package ConditionalAndLoops;
+
+public class WhileLoopJ {
+    public static void main(String[] args){
+        int count = 1;
+        while(count < 6 ){
+            System.out.println("Count of numbers: " + count);
+            count++;    // For while loop, we have to increment count manually.
+        }
+    }
+}
