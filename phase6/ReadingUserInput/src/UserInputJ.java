3dda65309e2454ba27bb09b68ee217caa14894ef
dunky-star <dukygeoffrey39@gmail.com>
2022-07-31 09:34:32 +0300

    User input - Java

--- phase6/ReadingUserInput/src/UserInputJ.java
+++ phase6/ReadingUserInput/src/UserInputJ.java
@@ -1,2 +1,16 @@
+/**
+ * Reading user inputs from keyboard.
+ */
+
+import java.util.Scanner;
+
 public class UserInputJ {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Your name is: " + name);
+        sc.close();
+
+    }
 }
