c355dba0adf1e8e80510d51330b2ba91dc3b2c90
dunky-star <dukygeoffrey39@gmail.com>
2022-07-31 13:24:46 +0300

    User input - Java

--- phase6/ReadingUserInput/src/UserInputJ.java
+++ phase6/ReadingUserInput/src/UserInputJ.java
@@ -1,5 +1,7 @@
 /**
  * Reading user inputs from keyboard.
+ * Program to calculate current age of a person in 2022 when that person
+ * enters his/her year of birth (hard coded though).
  */
 
 import java.util.Scanner;
@@ -7,9 +9,12 @@ import java.util.Scanner;
 public class UserInputJ {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = sc.nextInt();
         System.out.println("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Your name is: " + name);
+        int age = 2022 - yearOfBirth;
+        System.out.println("Your name is: " + name + " and age is: " +age);
         sc.close();
 
     }
