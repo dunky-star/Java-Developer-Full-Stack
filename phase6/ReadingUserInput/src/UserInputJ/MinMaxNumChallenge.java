79a4bd83ad9bc2ffdfddd686b856b3b396de7289
dunky-star <dukygeoffrey39@gmail.com>
2022-08-01 01:47:20 +0300

    User input - Java

--- phase6/ReadingUserInput/src/UserInputJ/MinMaxNumChallenge.java
+++ phase6/ReadingUserInput/src/UserInputJ/MinMaxNumChallenge.java
@@ -6,5 +6,33 @@
  */
 package UserInputJ;
 
+import java.util.Scanner;
+
 public class MinMaxNumChallenge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int minValue = 0;
+        int maxValue = 0;
+
+        while (true) {
+            System.out.println("Enter Number: ");
+            boolean isAnInteger = sc.hasNextInt();
+            if (isAnInteger) {
+                int number = sc.nextInt();
+                if(number > maxValue){
+                    maxValue = number;
+                }
+                if(number < minValue){
+                    minValue = number;
+                }
+
+            } else {
+                break;
+            }
+            sc.nextLine();
+        }
+        System.out.println("Min = " + minValue + " , Max = " + maxValue);
+        sc.close();
+    }
+
 }
