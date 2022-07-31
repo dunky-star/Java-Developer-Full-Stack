23e198c341c1e1b034d3cc39983fb039b3441a07
dunky-star <dukygeoffrey39@gmail.com>
2022-08-01 01:28:12 +0300

    User input - Java

---
+++ phase6/ReadingUserInput/src/UserInputJ/MinMaxNumChallenge.java
@@ -0,0 +1,10 @@
+/**
+ * Min and Max number challenge:
+ * Read the number from the console entered by the user, and print the minimum and maximum number.
+ * If the user enters an invalid number, break out of the loop and print
+ * the minimum and maximum number.
+ */
+package UserInputJ;
+
+public class MinMaxNumChallenge {
+}
