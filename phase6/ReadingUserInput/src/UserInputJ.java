5ad780e43b2c30f7129b25746665d886b76ca7cb
dunky-star <dukygeoffrey39@gmail.com>
2022-07-31 13:41:08 +0300

    User input - Java

--- phase6/ReadingUserInput/src/UserInputJ.java
+++ phase6/ReadingUserInput/src/UserInputJ.java
@@ -8,13 +8,19 @@ import java.util.Scanner;
 
 public class UserInputJ {
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your year of birth: ");
+
+        System.out.print("Enter your year of birth: ");
         int yearOfBirth = sc.nextInt();
-        System.out.println("Enter your name: ");
+        sc.nextLine();  // To handle next line character (Enter Key).
+
+        System.out.print("Enter your name: ");
         String name = sc.nextLine();
         int age = 2022 - yearOfBirth;
-        System.out.println("Your name is: " + name + " and age is: " +age);
+
+        System.out.println("Your name is: " + name + " and your age is: " + age);
+
         sc.close();
 
     }
