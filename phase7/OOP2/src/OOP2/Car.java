12126cd3acc1e10bdb6e467738dcfc8dd8f40931
dunky-star <dukygeoffrey39@gmail.com>
2022-08-01 04:35:14 +0300

    Object Oriented programming in Java

--- phase7/OOP2/src/OOP2/Car.java
+++ phase7/OOP2/src/OOP2/Car.java
@@ -1,6 +1,6 @@
 /**
  * Class is a blue-print for creating an object. Real World objects have state and behaviour.
- * Class variables are known as member variables and method variables are known as local variables.
+ * Class variables are known as member variables/ Instance variables and method variables are known as local variables.
  * Class variables are accessible anywhere within that class.
  * Local variables are accessible within that method body.
  */
@@ -11,8 +11,15 @@ public class Car {
     private int wheels;
     private String model;
     private String engine;
-    private String color ="Blue";
-    public void print(){
-        System.out.print("Car color: " + color);
+    private String color;
+
+    //Setter - making private variables accessible outside the class.
+    // This is the principle of encapsulation.
+    public void setModel(String model) {
+        this.model = model;
+    }
+    // Getter
+    public String getModel(){
+        return this.model;
     }
 }
--- phase7/OOP2/src/OOP2/Main.java
+++ phase7/OOP2/src/OOP2/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
         Car porsche = new Car();
         Car subaru = new Car();
-        subaru.print();
+        subaru.setModel("Legacy B4 Twin Turbo.");
+        System.out.println("The model is " + subaru.getModel());
     }
 }
