d0981ca2c48aacca78bfe70b6f7e2916f087536d
dunky-star <dukygeoffrey39@gmail.com>
2022-08-01 20:01:30 +0300

    Object Oriented programming in Java

--- phase7/OOP2/src/OOP2/BankAccount.java
+++ phase7/OOP2/src/OOP2/BankAccount.java
@@ -16,4 +16,43 @@ public class BankAccount {
     private String customerEmailAddress;
     private String customerPhoneNumber;
 
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
 }
