04bcde3df736a47d7330e56437477c1a1923ff18
dunky-star <dukygeoffrey39@gmail.com>
2022-08-01 19:58:20 +0300

    Object Oriented programming in Java

---
+++ phase7/OOP2/src/OOP2/BankAccount.java
@@ -0,0 +1,19 @@
+/**
+ * Written by: Geoffrey Duncan O. Tweet: dunky2012, Insta: KaligsXcross
+ * Create a new class for a bank account.
+ * Create fields for the account number, balance, customer name, email and phone number.
+ * Create getter and setter for each field. create two additional methods:
+ * 1. To allow the customer to deposit funds (this should increment the balance field).
+ * 2. To allow the customer to withdraw funds (this should deduct from the balance field).
+ * But not allow the withdrawal to complete if there is insufficient bal.
+ */
+package OOP2;
+
+public class BankAccount {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+}
---
+++ phase7/OOP2/src/OOP2/MainBankAccount.java
@@ -0,0 +1,7 @@
+package OOP2;
+
+public class MainBankAccount {
+    public static void main(String[] agrs){
+        BankAccount bankAccount = new BankAccount();
+    }
+}
