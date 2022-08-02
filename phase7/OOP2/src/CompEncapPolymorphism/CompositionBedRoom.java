a4238b5aa2177e47e59195105f2fefa0ee4d4c45
dunky-star <dukygeoffrey39@gmail.com>
2022-08-02 21:22:15 +0300

    OOP & composition in Java

---
+++ phase7/OOP2/src/CompEncapPolymorphism/CompositionBedRoom.java
@@ -0,0 +1,18 @@
+package CompEncapPolymorphism;
+
+/**
+ * Composition in Java
+ */
+
+public class CompositionBedRoom {
+    private String name;
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Ceiling ceiling;
+    private Bed bed;
+    private Lamp lamp;
+
+
+}
