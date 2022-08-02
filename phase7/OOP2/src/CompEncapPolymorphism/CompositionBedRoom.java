41bb33aac8a44d6fb424ca577cf2ff036cf4fbc1
dunky-star <dukygeoffrey39@gmail.com>
2022-08-02 21:38:57 +0300

    OOP & composition in Java

--- phase7/OOP2/src/CompEncapPolymorphism/CompositionBedRoom.java
+++ phase7/OOP2/src/CompEncapPolymorphism/CompositionBedRoom.java
@@ -1,7 +1,9 @@
 package CompEncapPolymorphism;
 
 /**
- * Composition in Java
+ * @author: Geoffrey Duncan O.
+ * @since: Java 17 (2022).
+ * Composition in Java.
  */
 
 public class CompositionBedRoom {
@@ -14,5 +16,25 @@ public class CompositionBedRoom {
     private Bed bed;
     private Lamp lamp;
 
+    // Constructor
 
+    public CompositionBedRoom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.bed = bed;
+        this.lamp = lamp;
+    }
+
+    // Getter
+    public Lamp getLamp(){
+        return this.lamp;
+    }
+    public void makeBed(){
+        System.out.println("Bedroom -> Making bed");
+        bed.make();
+    }
 }
