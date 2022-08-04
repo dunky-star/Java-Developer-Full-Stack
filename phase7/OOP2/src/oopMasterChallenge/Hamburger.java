34e1d10edd8c7d8a5ba953a5c8a75db5ab8a6935
dunky-star <dukygeoffrey39@gmail.com>
2022-08-04 17:07:36 +0300

    Wrapping up all concepts of OOP into one

--- phase7/OOP2/src/oopMasterChallenge/Hamburger.java
+++ phase7/OOP2/src/oopMasterChallenge/Hamburger.java
@@ -26,29 +26,45 @@ public class Hamburger {
         this.breadRollType = breadRollType;
     }
 
-    public void addHamburgerAddition1(String name, double price){
+    public void addHamburgerAddition1(String name, double price) {
         this.addition1Name = name;
         this.addition1Price = price;
     }
-    public void addHamburgerAddition2(String name, double price){
+
+    public void addHamburgerAddition2(String name, double price) {
         this.addition2Name = name;
         this.addition2Price = price;
     }
-    public void addHamburgerAddition3(String name, double price){
+
+    public void addHamburgerAddition3(String name, double price) {
         this.addition3Name = name;
         this.addition3Price = price;
     }
-    public void addHamburgerAddition4(String name, double price){
+
+    public void addHamburgerAddition4(String name, double price) {
         this.addition4Name = name;
         this.addition4Price = price;
     }
-    public double itemizeHamburger(){
+
+    public double itemizeHamburger() {
         double hamburgerPrice = this.price;
         System.out.println(this.name + " hamburger " + " on a " + this.breadRollType + " roll "
-                            + "price is " + this.price);
-        if(this.addition1Name != null){
-            hamburgerPrice += this.addition1Price;
+                + "price is " + this.price);
+        if (this.addition1Name != null) {
+             hamburgerPrice += this.addition1Price;
             System.out.println("Added " + this.addition1Name + " for an extra " + this.addition1Price);
+        }
+        if(this.addition2Name != null){
+            hamburgerPrice += this.addition1Price;
+            System.out.println("Added " + this.addition2Name + " for an extra " + this.addition2Price);
+        }
+        if(this.addition3Name != null){
+            hamburgerPrice += this.addition3Price;
+            System.out.println("Added " + this.addition3Name + " for an extra " + this.addition3Price);
+        }
+        if(this.addition4Name != null){
+            hamburgerPrice += this.addition4Price;
+            System.out.println("Added " + this.addition4Name + " for an extra " + this.addition4Price);
         }
     }
 }
