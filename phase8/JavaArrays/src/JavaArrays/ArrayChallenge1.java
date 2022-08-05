a7e0c8def255d34dd705dc645e79e2d3aa8688bf
dunky-star <dukygeoffrey39@gmail.com>
2022-08-05 07:53:13 +0300

    Arrays, Java inbuilt Lists, Autoboxing and Unboxing

---
+++ phase8/JavaArrays/src/JavaArrays/ArrayChallenge1.java
@@ -0,0 +1,11 @@
+package JavaArrays;
+
+/**
+ * @author: Geoffrey Duncan O.
+ * Arrays allow us to store multiple values of the same data type.
+ * It's a data structure that allows you to store sequence of values that hold same type.
+ * Eg: An array of integers, an array of characters, an array of Strings, etc.
+ */
+
+public class ArrayChallenge1 {
+}
