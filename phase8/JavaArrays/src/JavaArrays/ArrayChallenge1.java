b6fc0f01518dc19f88f752b01dffe29021528e1d
dunky-star <dukygeoffrey39@gmail.com>
2022-08-05 09:29:12 +0300

    Arrays, Java inbuilt Lists, Autoboxing and Unboxing

--- phase8/JavaArrays/src/JavaArrays/ArrayChallenge1.java
+++ phase8/JavaArrays/src/JavaArrays/ArrayChallenge1.java
@@ -9,4 +9,5 @@ package JavaArrays;
  */
 
 public class ArrayChallenge1 {
+
 }
--- phase8/JavaArrays/src/JavaArrays/ArrayIntroduction.java
+++ phase8/JavaArrays/src/JavaArrays/ArrayIntroduction.java
@@ -1,6 +1,5 @@
 package JavaArrays;
 
-
 /**
  * @author: Geoffrey Duncan O.
  * Arrays allow us to store multiple values of the same data type.
@@ -11,7 +10,18 @@ package JavaArrays;
 
 public class ArrayIntroduction {
     public static void main(String[] args){
-        int[] myIntArray = new int[10];
-        myIntArray[5] = 50;
+        int[] myIntArray = new int[5];
+        myIntArray[0] = 10;
+        myIntArray[1] = 20;
+        myIntArray[2] = 30;
+        myIntArray[3] = 40;
+        myIntArray[4] = 50;
+        double[] myDoubleArray = new double[5];
+        System.out.println("The array at index[0] is " + myIntArray[0] + "\n");
+        System.out.println("===========================");
+
+        for(int i = 0; i < myIntArray.length; i++){
+        System.out.println("The array at index# " + i + " is " + myIntArray[i]);
+        }
     }
 }
