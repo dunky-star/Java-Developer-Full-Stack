86dc999c21321d1032c864bb9155418c175bb337
dunky-star <dukygeoffrey39@gmail.com>
2022-08-05 12:47:27 +0300

    Arrays, Java inbuilt Lists, Autoboxing and Unboxing

---
+++ phase8/JavaArrays/src/JavaArrays/ArrayInputAverage.java
@@ -0,0 +1,39 @@
+package JavaArrays;
+/**
+ * Write a program that accepts 5 numbers from a keyboard input then store them in an array.
+ * Finally, get the average of those numbers.
+ */
+
+import java.util.Scanner;
+
+public class ArrayInputAverage {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is: " + getAverage(myIntegers));
+    }
+
+    // Method for getting values from the keyboard.
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values:\r");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = sc.nextInt();
+        }
+        return values;
+    }
+
+    // Method for calculating the average
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
+    }
+
+}
