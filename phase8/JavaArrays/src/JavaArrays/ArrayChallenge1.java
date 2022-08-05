a954f8ac64b3992479e31fc1b6ca2570742f395e
dunky-star <dukygeoffrey39@gmail.com>
2022-08-05 16:59:50 +0300

    Arrays, Java inbuilt Lists, Autoboxing and Unboxing

--- phase8/JavaArrays/src/JavaArrays/ArrayChallenge1.java
+++ phase8/JavaArrays/src/JavaArrays/ArrayChallenge1.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 /**
  * @author: Geoffrey Duncan Opiyo
  * Create a program using arrays that sorts a list of integers in descending order.
- *  Set up the program so that the numbers to sort are read in from the keyboard.
- *  Implement the following methods - getIntegers, printArray, and sortIntegers.
- *  getIntegers returns an array of entered integers from keyboard.
- *  printArray prints out the contents of the array
- *  and sortIntegers should sort the array and return a new array containing the sorted numbers
+ * Set up the program so that the numbers to sort are read in from the keyboard.
+ * Implement the following methods - getIntegers, printArray, and sortIntegers.
+ * getIntegers returns an array of entered integers from keyboard.
+ * printArray prints out the contents of the array
+ * and sortIntegers should sort the array and return a new array containing the sorted numbers
  * you will have to figure out how to copy the array elements from the passed array into a new array and sort them and return the new sorted array.
  */
 
@@ -17,7 +17,7 @@ public class ArrayChallenge1 {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] myIntegers = getIntegers(10);
+        int[] myIntegers = getIntegers(5);
         // sorting method call
         int[] sorted = sortIntegers(myIntegers);
         // Displaying result by printArray call
@@ -37,25 +37,24 @@ public class ArrayChallenge1 {
     }
 
     // Method to print the array
-    public static void printArray(int[] array){
-        for(int i = 0; i < array.length; i++){
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
             System.out.println("Element " + i + " content is " + array[i]);
         }
     }
-    public static int[] sortIntegers(int[] array){
+
+    public static int[] sortIntegers(int[] array) {
         int[] sortedArray = new int[array.length];
-        for (int i = 0; i < array.length; i++){
-            sortedArray[i] = array[i];
-        }
+        System.arraycopy(array, 0, sortedArray, 0, array.length);
         boolean flag = true;
         int temp;
-        while(flag){
+        while (flag) {
             flag = false;
-            for(int i =0; i < sortedArray.length-1; i++){
-                if(sortedArray[i] < sortedArray[i+1]){
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
                     temp = sortedArray[i];
-                    sortedArray[i] = sortedArray[i+1];
-                    sortedArray[i+1] = temp;
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
                     flag = true;
                 }
             }
