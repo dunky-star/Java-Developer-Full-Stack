4dc5dbd9536599ddca00a336b6e25a49fe6334d0
dunky-star <dukygeoffrey39@gmail.com>
2022-08-30 01:12:26 +0300

    Setup Tomcat Datasource for Connection Pooling

--- Mini-App-MVC/mini-app-Servlets/src/com/dunky/web/jdbc/TestServlet.java
+++ Mini-App-MVC/mini-app-Servlets/src/com/dunky/web/jdbc/TestServlet.java
@@ -15,60 +15,50 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 /**
- * Servlet implementation class test_servlet
+ * Servlet implementation class TestServlet
  */
-@WebServlet("/test_servlet")
-public class test_servlet extends HttpServlet {
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	
-    
-	// Step 0: Define datasource/connection pool for Resource Injection
-	
+
+	// Define datasource/connection pool for Resource Injection
 	@Resource(name="jdbc/web_student_j")
 	private DataSource dataSource;
-
-    /**
-     * Default constructor. 
-     */
-    public test_servlet() {
-        // TODO Auto-generated constructor stub
-    }
-
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+
 		// Step 1:  Set up the printwriter
 		PrintWriter out = response.getWriter();
-		response.setContentType("text/plain");	
+		response.setContentType("text/plain");
 		
 		// Step 2:  Get a connection to the database
 		Connection myConn = null;
 		Statement myStmt = null;
-		ResultSet MyRs = null;
+		ResultSet myResultSet = null;
 		
 		try {
-			
 			myConn = dataSource.getConnection();
 			
-			// Step 3: Create a SQL statements
+			// Step 3:  Create a SQL statements
 			String sql = "select * from student";
 			myStmt = myConn.createStatement();
 			
-			// Step 4: Execute SQL query
-			MyRs = myStmt.executeQuery(sql);
+			// Step 4:  Execute SQL query
+			myResultSet = myStmt.executeQuery(sql);
 			
-			// Step 5: Process the result set
-			while (MyRs.next()) {
-				String last_name = MyRs.getString("last_name");
+			// Step 5:  Process the result set
+			while (myResultSet.next()) {
+				String last_name = myResultSet.getString("last_name");
 				out.println(last_name);
 			}
 		}
-		
-	} catch (Exception exc) {
-		exc.printStackTrace();
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 
 }
