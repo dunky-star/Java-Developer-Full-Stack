a28fb1de85ed8b602dc13ae3f6c2a90cab682cd2
dunky-star <dukygeoffrey39@gmail.com>
2022-09-03 16:09:20 +0300

    Integrating Lombok to Spring-Boot tool

---
+++ Spring-Boot/eCommerceModel/src/main/java/com/dunky/yohunters/ECommerceModelApplication.java
@@ -0,0 +1,13 @@
+package com.dunky.yohunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceModelApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot/eCommerceModel/src/test/java/com/dunky/yohunters/ECommerceModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunky.yohunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
