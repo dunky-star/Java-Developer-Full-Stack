a8a0de71e6481c8e4f999febbafcd33fc6e5f2ab
dunky-star <dukygeoffrey39@gmail.com>
2022-09-13 17:08:48 +0300

    Create Main class to test the OneToOne Hibernate Mapping

---
+++ Spring-Hibernate-ORM-JPA/hibernate-crud-onetoone/src/main/java/MyApp.java
@@ -0,0 +1,11 @@
+import com.dunky.hibernate.dao.InstructorDao;
+import com.dunky.hibernate.entity.Instructor;
+import com.dunky.hibernate.entity.InstructorDetail;
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
