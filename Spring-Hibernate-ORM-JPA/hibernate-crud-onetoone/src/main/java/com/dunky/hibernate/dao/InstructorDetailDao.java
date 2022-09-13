f8dfa13e06641e886d096fe14a14df0992e05511
dunky-star <dukygeoffrey39@gmail.com>
2022-09-13 16:30:00 +0300

    Create InstructorDetail Data Access Object

---
+++ Spring-Hibernate-ORM-JPA/hibernate-crud-onetoone/src/main/java/com/dunky/hibernate/dao/InstructorDetailDao.java
@@ -0,0 +1,13 @@
+package com.dunky.hibernate.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.dunky.hibernate.entity.InstructorDetail;
+import com.dunky.hibernate.util.HibernateUtil;
+
+public class InstructorDetailDao {
+	
+	
+
+}
