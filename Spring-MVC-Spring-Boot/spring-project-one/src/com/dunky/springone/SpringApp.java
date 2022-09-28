29ea247c86e7bad965f2ecc837ed3bbe3cb0631d
dunky-star <dukygeoffrey39@gmail.com>
2022-09-28 18:06:13 +0300

    Spring project - IoC

---
+++ Spring-MVC-Spring-Boot/spring-project-one/src/com/dunky/springone/SpringApp.java
@@ -0,0 +1,23 @@
+package com.dunky.springone;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+				
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+				
+		// close the context
+		context.close();
+
+	}
+
+}
