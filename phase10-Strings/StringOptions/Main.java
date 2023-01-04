d07299c1eadf4e431a339330ed56359cff3b5ac7
dunky-star <dukygeoffrey39@gmail.com>
2023-01-04 19:35:35 +0300

    Manipulating strings in Java with String Builder

--- phase10-Strings/StringOptions/Main.java
+++ phase10-Strings/StringOptions/Main.java
@@ -3,13 +3,20 @@ package StringOptions;
 public class Main {
     public static void main(String[] args){
 
-        String helloWorld = "Hello" + "World";
+        String helloWorld = "Hello" + " World";
+        helloWorld.concat(" and Goodbye");
 
-        StringBuilder stringBuilder = new StringBuilder("Hello" + "World");
+        StringBuilder stringBuilder = new StringBuilder("Hello" + " World");
+        stringBuilder.append(" and Goodbye");
+        stringBuilder.deleteCharAt(4).insert(9,"d");
+
+        StringBuilder emptyBuilder = new StringBuilder();
+        emptyBuilder.append("a".repeat(17));
 
         // Call the methods onto these variables.
         printInformation(helloWorld);
         printInformation(stringBuilder);
+        printInformation(emptyBuilder);
 
     }
 
