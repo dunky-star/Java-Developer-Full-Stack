f3476058b4028246af3c3e89b0c652c711c7aad6
dunky-star <dukygeoffrey39@gmail.com>
2023-01-04 20:52:19 +0300

    Array data structure

--- phase8-Arrays/JavaArrays/src/JavaArrays/ArrayIntroduction.java
+++ phase8-Arrays/JavaArrays/src/JavaArrays/ArrayIntroduction.java
@@ -11,25 +11,30 @@ package JavaArrays;
 
 public class ArrayIntroduction {
     public static void main(String[] args){
-        int[] myIntArray = new int[10];
+        // Array declaration with an array initializer.
+        int[] myIntArray = new int[]{1,2,3,4,5,6,7,8,9,20};
 
         myIntArray[0] = 10;
         myIntArray[1] = 20;
         myIntArray[2] = 30;
         myIntArray[3] = 40;
         myIntArray[4] = 50;
+        for (int k = 0 ; k < myIntArray.length; k++) {
+            System.out.println("My int array are: " + myIntArray[k]);
+        }
+        System.out.println("============================");
 
         // Alternatively. Array can be defined initialized with a for loop:
-        int[] myIntegerArr = new int[20];
+        int[] myIntegerArr = new int[20]; // 20 is the array size/length.
         for (int i = 0; i < myIntegerArr.length; i++) {
-            myIntegerArr[i] = i * 20;
+            myIntegerArr[i] = myIntegerArr.length - i;
         }
         printArray(myIntegerArr); // print method call.
 
 
         // Alternatively. Array can be defined directly with curly braces as:
         double[] myDoubleArray = {1.0, 2.0, 3.0, 4.0, 5.0, 60.0, 70.0,80.0, 90.5};
-        String[] myStringArray = {"Duncan", "Kaligs", "Kei", "Boni", "Deus"};
+        String[] myStringArray = new String[]{"Duncan", "Kaligs", "Kei", "Boni", "Deus"};
 
         System.out.println("The myDoubleArray at index[5] is " + myDoubleArray[5] + "\n");
         System.out.println("===========================");
