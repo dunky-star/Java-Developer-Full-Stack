a589bdf945462401323514c3b4c6f1fbf81af2f3
dunky-star <dukygeoffrey39@gmail.com>
2023-01-05 13:16:22 +0300

    ArrayList and LinkedList

--- phase8-List[ArrayList]/ArrayList1.java
+++ phase8-List[ArrayList]/ArrayList1.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+
 /**
  * @author: Geoffrey Duncan O.
- * ArrayList is a growable array.
+ * ArrayList is a growable array. i.e: resizeable.
  */
 
-record GroceryList( ){
-
+record GroceryItem(String name, String type, int count){
+    public GroceryItem(String name){
+        this(name, "Dairy", 1);
+    }
 }
 
 public class ArrayList1 {
     public static void main(String[] args){
-
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter", "Nun", 10 ));
+        groceryList.add(new GroceryItem("Rice", "Bishimati", 5 ));
+        groceryList.add(new GroceryItem("Meat"));
     }
 }
