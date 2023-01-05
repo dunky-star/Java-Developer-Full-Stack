c7ebec3c1c75456b525c9c94a96e8d7cd4475a4e
dunky-star <dukygeoffrey39@gmail.com>
2023-01-05 18:18:08 +0300

    ArrayList challenge

--- phase8-List[ArrayList]/ArrayListChallenge.java
+++ phase8-List[ArrayList]/ArrayListChallenge.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 /**
@@ -16,16 +18,46 @@ public class ArrayListChallenge {
 
     public static void main(String[] args){
 
+        ArrayList<String> groceries = new ArrayList<>();
         boolean flag = true;
+
         while(flag){
             // Calling the print method for user actions required.
             printActions();
-            switch(Integer.parseInt(sc.nextLine())){
-                default -> flag = true;
-            }
 
+            switch(Integer.parseInt(sc.nextLine())){
+                case 1 -> addItems(groceries);
+                case 2 -> removeItems(groceries);
+                default -> flag = false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
         }
 
+    }
+
+    // Method that add the groceries list items
+    private static void addItems(ArrayList<String> groceries){
+        System.out.println("Add item(s) [separate items by comma]:");
+        String[] items = sc.nextLine().split(",");
+
+        for (String i: items) {
+            String trimmed = i.trim();
+            if (groceries.indexOf(trimmed) < 0) {
+                groceries.add(trimmed);
+            }
+        }
+
+    }
+
+    // Method that add the groceries list items
+    private static void removeItems(ArrayList<String> groceries){
+        System.out.println("Remove item(s) [separate items by comma]:");
+        String[] items = sc.nextLine().split(",");
+                for (String i: items) {
+                   String trimmed = i.trim();
+                   groceries.remove(trimmed);
+                }
 
     }
 
