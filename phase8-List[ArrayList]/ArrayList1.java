2bc352fecf6554f97a0b061f81583db238878c3d
dunky-star <dukygeoffrey39@gmail.com>
2023-01-05 13:01:01 +0300

    ArrayList and LinkedList

---
+++ phase8-List[ArrayList]/ArrayList1.java
@@ -0,0 +1,14 @@
+/**
+ * @author: Geoffrey Duncan O.
+ * ArrayList is a growable array.
+ */
+
+record GroceryList( ){
+
+}
+
+public class ArrayList1 {
+    public static void main(String[] args){
+
+    }
+}
