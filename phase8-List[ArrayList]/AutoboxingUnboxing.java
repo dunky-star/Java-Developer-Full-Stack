0ac976a2753429386a9c588b9fb4cbed7ced4de3
dunky-star <dukygeoffrey39@gmail.com>
2023-01-13 12:25:34 +0300

    Autoboxing and Unboxing in Java

---
+++ phase8-List[ArrayList]/AutoboxingUnboxing.java
@@ -0,0 +1,15 @@
+/**
+ * @author: Geoffrey Duncan O.
+ * Autoboxing: Go from a primitive data type to wrapper class.
+ * Unboxing: Or a wrapper class to primitive data type with relative ease in Java.
+ */
+
+public class AutoboxingUnboxing {
+    public static void main(String[] args){
+        // Autoboxing
+        Integer boxedInt = 15;
+
+        // Unboxing
+        int unboxedInt = boxedInt;
+    }
+}
