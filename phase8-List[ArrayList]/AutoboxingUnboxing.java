52582eb1ab83049af190a59a73fbc8c29993047f
dunky-star <dukygeoffrey39@gmail.com>
2023-01-13 16:47:35 +0300

    Autoboxing and Unboxing in Java

--- phase8-List[ArrayList]/AutoboxingUnboxing.java
+++ phase8-List[ArrayList]/AutoboxingUnboxing.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+
 /**
  * @author: Geoffrey Duncan O.
  * Autoboxing: Go from a primitive data type to wrapper class.
  * Unboxing: Or a wrapper class to primitive data type with relative ease in Java.
+ *
+ * Autoboxing challenge --- banking case study (customer's transaction).
  */
+
+record Customer(String name, ArrayList<Double> transactions) {
+
+    public Customer(String name, double initialDeposit) {
+        this(name.toUpperCase(),
+                new ArrayList<Double>(500));
+        transactions.add(initialDeposit);
+    }
+}
 
 public class AutoboxingUnboxing {
     public static void main(String[] args){
@@ -11,5 +24,85 @@ public class AutoboxingUnboxing {
 
         // Unboxing
         int unboxedInt = boxedInt;
+        Customer bob = new Customer("Bob S", 1000.0);
+        System.out.println(bob);
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("jane A", -75.01);
+        bank.printStatement("Jane a");
+
+        bank.addNewCustomer("bob s", 25);
+        bank.addTransaction("Bob S", 100);
+        bank.printStatement("Bob S");
+
     }
+
+    static class Bank {
+
+        private String name;
+        private ArrayList<Customer> customers = new ArrayList<>(5000);
+
+        public Bank(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Bank{" +
+                    "name='" + name + '\'' +
+                    ", customers=" + customers +
+                    '}';
+        }
+
+        private Customer getCustomer(String customerName) {
+
+            for (var customer : customers) {
+                if (customer.name().equalsIgnoreCase(customerName)) {
+                    return customer;
+                }
+            }
+            System.out.printf("Customer (%s) wasn't found %n", customerName);
+
+            return null;
+        }
+
+        public void addNewCustomer(String customerName, double initialDeposit) {
+
+            if (getCustomer(customerName) == null) {
+                Customer customer = new Customer(customerName, initialDeposit);
+                customers.add(customer);
+                System.out.println("New Customer added: " + customer);
+            }
+        }
+
+        public void addTransaction(String name, double transactionAmount) {
+
+            Customer customer = getCustomer(name);
+            if (customer != null) {
+                customer.transactions().add(transactionAmount);
+            }
+        }
+
+        public void printStatement(String customerName) {
+
+            Customer customer = getCustomer(customerName);
+            if (customer == null) {
+                return;
+            }
+
+            System.out.println("-".repeat(30));
+            System.out.println("Customer Name: " + customer.name());
+            System.out.println("Transactions:");
+            for (double d : customer.transactions()) {  // unboxing
+                System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+            }
+
+        }
+
+    }
+
 }
