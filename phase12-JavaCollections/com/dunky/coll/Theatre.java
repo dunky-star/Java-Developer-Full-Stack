848074fa0c096751a90536c9bbf305c5ab34e3c5
dunky-star <dukygeoffrey39@gmail.com>
2023-01-14 11:02:14 +0300

    Java Collections

--- phase12-JavaCollections/com/dunky/coll/Theatre.java
+++ phase12-JavaCollections/com/dunky/coll/Theatre.java
@@ -1,11 +1,11 @@
 package com.dunky.coll;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    private Collection<Seat> seats = new LinkedList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
