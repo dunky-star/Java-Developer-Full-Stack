f61e51894a89bf1d99f880cda3f0a370348e0b36
dunky-star <dukygeoffrey39@gmail.com>
2023-01-14 12:44:37 +0300

    Java Collections - Movie Theater seats case study

--- phase12-JavaCollections/com/dunky/coll/Theatre.java
+++ phase12-JavaCollections/com/dunky/coll/Theatre.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
@@ -53,7 +53,7 @@ public class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
