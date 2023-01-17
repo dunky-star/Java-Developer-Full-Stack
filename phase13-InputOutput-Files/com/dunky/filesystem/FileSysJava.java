9014a0f8d3fe17957880bddb49b429ada8f3b021
dunky-star <dukygeoffrey39@gmail.com>
2023-01-17 13:34:57 +0300

    Working with Java file systems

--- phase13-InputOutput-Files/com/dunky/filesystem/FileSysJava.java
+++ phase13-InputOutput-Files/com/dunky/filesystem/FileSysJava.java
@@ -2,25 +2,14 @@ package com.dunky.filesystem;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class FileSysJava {
     public static void main(String[] args){
 
         Path filePath = Paths.get("D:\\", "Examples", "file1.txt");
 
-        // Copying file from one directory to another.
-        try {
-            System.out.println("File created is: " + Files.createFile(filePath));     //creates file at specified location.
-            Path sourceFile = FileSystems.getDefault().getPath("D:\\", "Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("D:\\", "Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile);
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+
 
 //        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
 //        printFile(path);
@@ -55,4 +44,45 @@ public class FileSysJava {
             e.printStackTrace();
         }
     }
+
+    private static void deleteFile(Path path) {
+        try {
+            // Deleting a file
+            Path fileToDelete = FileSystems.getDefault().getPath("D:\\", "Examples", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void moveFile(Path path) {
+        try {
+            // Moving a file
+            Path fileToMove = FileSystems.getDefault().getPath("D:\\", "Examples", "file1copy.txt");
+            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1","file1copy.txt");
+            Files.move(fileToMove, destination);
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void createFile(Path path) {
+        Path filePath = Paths.get("D:\\", "Examples", "file1.txt");
+        try {
+            System.out.println("File created is: " + Files.createFile(filePath));     //creates file at specified location.
+            // Copying file from one directory to another.
+            Path sourceFile = FileSystems.getDefault().getPath("D:\\", "Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("D:\\", "Examples", "file1copy.txt");
+            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
