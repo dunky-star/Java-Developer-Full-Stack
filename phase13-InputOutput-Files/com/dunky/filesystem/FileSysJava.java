1eb248c186c4c6bf9efdfb6bf9793a5f93071930
dunky-star <dukygeoffrey39@gmail.com>
2023-01-17 12:58:28 +0300

    Working with Java file systems

--- phase13-InputOutput-Files/com/dunky/filesystem/FileSysJava.java
+++ phase13-InputOutput-Files/com/dunky/filesystem/FileSysJava.java
@@ -10,24 +10,36 @@ import java.nio.file.Paths;
 public class FileSysJava {
     public static void main(String[] args){
 
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
+        Path filePath = Paths.get("D:\\", "Examples", "file1.txt");
 
-        // Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
-        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
-        printFile(filePath);
+        // Copying file from one directory to another.
+        try {
+            System.out.println("File created is: " + Files.createFile(filePath));     //creates file at specified location.
+            Path sourceFile = FileSystems.getDefault().getPath("D:\\", "Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("D:\\", "Examples", "file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
-        filePath = Paths.get("D:\\", "Examples", "OutThere.txt");
-        // D:\\Examples\\OutThere.txt
-        printFile(filePath);
-
-        // Getting and displaying file path.
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath());
-
-        // Checking if files/directory exist.
-        Path filePathCheck = FileSystems.getDefault().getPath("files");
-        System.out.println("Exists = " + Files.exists(filePath));
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+//
+//        // Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+//        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get("D:\\", "Examples", "OutThere.txt");
+//        // D:\\Examples\\OutThere.txt
+//        printFile(filePath);
+//
+//        // Getting and displaying file path.
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        // Checking if files/directory exist.
+//        Path filePathCheck = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
 
     }
 
