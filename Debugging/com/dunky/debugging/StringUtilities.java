2f950e45606439635174c3005355325b439116df
dunky-star <dukygeoffrey39@gmail.com>
2023-01-23 17:43:15 +0300

    Java debugging

--- Debugging/com/dunky/debugging/StringUtilities.java
+++ Debugging/com/dunky/debugging/StringUtilities.java
@@ -4,7 +4,7 @@ public class StringUtilities {
     private StringBuilder sBuilder = new StringBuilder();
     private int charsAdded = 0;
     public void addChar(StringBuilder sBuilder, char c) {
-        this.sBuilder.append(c);
+        sBuilder.append(c);
         charsAdded++;
     }
 }
