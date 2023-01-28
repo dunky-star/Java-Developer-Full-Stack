4c5bba700fa80ef8a5b8a469d8aa8ef6ad8c52e3
dunky-star <dukygeoffrey39@gmail.com>
2023-01-28 15:30:37 +0300

    Spring reactive stream programming

--- spring-reactive-streams/src/test/java/com/dunky/stream/ApplicationTests.java
+++ spring-reactive-streams/src/test/java/com/dunky/stream/ApplicationTests.java
@@ -2,9 +2,20 @@ package com.dunky.stream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ApplicationTests {
+	@Test
+	public void shouldStreamSingleValue() throws Exception{
+		Mono.just("Hello")
+				.map(String::toUpperCase)
+				.doOnSubscribe(subscription -> System.out.println("==Subscribed"))
+				.doOnRequest(request -> System.out.println("==Requested"))
+				.doOnSuccess(complete -> System.out.println("==Stream completed"))
+				.log()
+				.subscribe();
+	}
 
 	@Test
 	void contextLoads() {
